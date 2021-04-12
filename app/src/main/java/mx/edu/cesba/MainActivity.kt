a8ac9424675b80a6114ab7b471503b50bda38a17
GazApp <juangaz@hotmail.com>
2021-04-12 00:44:45 -0500

    version 1.1 1. add class Network 2. add metodo volley 3. add if .statusRed

--- app/src/main/java/mx/edu/cesba/MainActivity.kt
+++ app/src/main/java/mx/edu/cesba/MainActivity.kt
@@ -2,8 +2,14 @@ package mx.edu.cesba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +26,18 @@ class MainActivity : AppCompatActivity() {
         viewestatus = findViewById(R.id.txt_status)
 
         val ciudad = intent.getStringExtra("mx.edu.cesba")
+
+        // 203:
+        if(Network.statusRed(this)){
+            //EJECUTAR SOLICITUD HTTPP (despues de pegar el metodo volley)
+            solicitudHTTPVolley(url = "https://api.openweathermap.org/data/2.5/weather?id=3530597&appid=300893205b07544c7c461e47973d610d")
+
+
+        }else{
+            // MOSTRAR MENSAJE DE ERROR:
+
+
+        }
 
         val cdmx = Ciudad("Ciudad de México", 18, "Poco Sol")
         val qro = Ciudad("Ciudad de Quéretaro", 32, "Nubleado")
@@ -41,4 +59,55 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    //205: metodo volley
+
+    private fun solicitudHTTPVolley(url: String) {
+        val queue = Volley.newRequestQueue(this)
+
+        val solicitud = StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
+
+            try {
+                Log.d("Solictud a OPENWETHER", response)
+
+            } catch (e: Exception) {
+
+            }
+        }, Response.ErrorListener { })
+
+        queue.add(solicitud)
+    }
 }
+
+// 201:
+// https://openweathermap.org/
+// https://openweathermap.org/api
+// https://openweathermap.org/price
+// https://home.openweathermap.org/api_keys
+// KEY : 300893205b07544c7c461e47973d610d
+
+// 202:
+// CREAR LA CLASE NETWORK.KT
+// PEGAR EL CODIGO DE LA APP getHttp
+// OTORAGAR LOS PERMSIOS EN EL MANIFIESTO
+
+//203: if
+
+// 204:
+//compile 'com.android.volley:volley:1.1.0'
+
+//compile 'com.squareup.okhttp3:okhttp:3.6.0'
+
+//205: metodo volley --COPIAR
+
+// BUSCAR LA DIRECCION EN OPENWEATER PARA MEXICO:
+// https://openweathermap.org/city/3530597
+// ID = 3530597
+
+//https://openweathermap.org/api
+//https://openweathermap.org/price
+//https://openweathermap.org/currentç
+
+// By city ID:
+//3530597
+
+// https://api.openweathermap.org/data/2.5/weather?id=3530597&appid=300893205b07544c7c461e47973d610d
